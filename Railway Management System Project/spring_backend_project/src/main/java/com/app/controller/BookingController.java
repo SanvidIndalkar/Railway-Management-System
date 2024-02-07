66b74bfe4503f9e77ce6db1e7e1f4d0c4c08cf99
SanvidIndalkar <sanvidmindalkar@gmail.com>
2024-02-07 15:49:16 +0530

    testing

--- Railway Management System Project/spring_backend_project/src/main/java/com/app/controller/BookingController.java
+++ Railway Management System Project/spring_backend_project/src/main/java/com/app/controller/BookingController.java
@@ -1,5 +1,6 @@
 package com.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class BookingController {
 	@GetMapping("/passengers")
 	public String getPassengersList(){
 
+		
 		return "";
 	}
 	
