287b6feb2f0c3729d50364660a8d91f6db7c6aa9
SanvidIndalkar <sanvidmindalkar@gmail.com>
2024-03-03 17:20:49 +0530

    seats

--- Railway Management System Project/spring_backend_project/src/main/java/com/app/dao/SeatAvailabiltyDao.java
+++ Railway Management System Project/spring_backend_project/src/main/java/com/app/dao/SeatAvailabiltyDao.java
@@ -1,6 +1,9 @@
 package com.app.dao;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import com.app.entities.Seat;
 import com.app.entities.SeatAvailability;
@@ -8,6 +11,7 @@ import com.app.entities.Stop;
 
 public interface SeatAvailabiltyDao extends JpaRepository<SeatAvailability, Long>{
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	SeatAvailability findByStopAndSeat(Stop stop, Seat seat);
 
 	
